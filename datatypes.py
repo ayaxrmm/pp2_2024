de0e0ed6aa90e9f25910147394d6038288452ee0
Ayaulym <raimbekayaulim04@mail.ru>
2024-01-21 22:39:56 +0600

    commit

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+#ex1
+x = 5
+print(type(x))  #type - int
+
+#ex2
+x = "Hello World"
+print(type(x))  #type - str
+
+#ex3
+x = 20.5
+print(type(x)) #type - float
+
+#ex4
+x = ["apple", "banana", "cherry"]
+print(type(x))  #type - list
+
+#ex5
+x = ("apple", "banana", "cherry")
+print(type(x)) #type - tuple
+
+#ex6
+x = {"name" : "John", "age" : 36}
+print(type(x)) #type - dict
+
+#ex7
+x = True
+print(type(x)) #type - bool
+
