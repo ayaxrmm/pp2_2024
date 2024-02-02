60f1e555e31bd201fbd23d5029463c0d028f9185
Ayaulym <raimbekayaulim04@mail.ru>
2024-02-02 12:33:54 +0600

    Added some changes

--- lab3/classes/examples.py
+++ lab3/classes/examples.py
@@ -27,7 +27,64 @@ class Person:
   def __init__(self, name, age):
     self.name = name
     self.age = age
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+p1=Person("John", 36)
+p1.myfunc()
+
 #5 example
-#6 example
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+#6example
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+
+p1.age = 40
+
+print(p1.age)
+
 #7 example
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+
+del p1.age
+
+print(p1.age)
+
 #8 example
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+
+del p1
+
+print(p1)
