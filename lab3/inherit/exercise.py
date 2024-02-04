be0dc64905ebab0a90b60faac8260127a6fb816d
Ayaulym <raimbekayaulim04@mail.ru>
2024-02-04 20:22:53 +0600

    upd

---
+++ lab3/inherit/exercise.py
@@ -0,0 +1,16 @@
+#1 task
+#class student(Person):
+
+#2task
+class Person:
+  def __init__(self, fname):
+    self.firstname = fname
+
+  def printname(self):
+    print(self.firstname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike")
+x.printname()
